fe8460fd1e358a2c1e53e862ffdadf5e087c2fb7
Basile Starynkevitch <basile@starynkevitch.net>
2011-02-16 21:44:37 +0100

    added copyright & GPLv3 to first.c

--- module/first.c
+++ module/first.c
@@ -1,4 +1,23 @@
 // file module/first.c
+/****
+  © 2011  Basile Starynkevitch
+
+    this file first.c is part of IaCa
+
+    IaCa is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    IaCa is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with IaCa.  If not, see <http://www.gnu.org/licenses/>.
+****/
+
 #include "iaca.h"
 
 /// initialize the application
