a2f7229fc3d38ca895f6a4af827ea5c713a91fd4
Basile Starynkevitch <basile@starynkevitch.net>
2016-02-19 23:03:59 +0100

    trying again with Qt & JsonCpp in mind

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 ## file Iaca/Makefile
-## © 2012 Basile Starynkevitch 
+## © 2016 Basile Starynkevitch 
 ##   this file Makefile is part of IaCa 
 ##   IaCa is free software: you can redistribute it and/or modify
 ##   it under the terms of the GNU General Public License as published by
@@ -17,13 +17,16 @@
 CXX=g++
 ASTYLE=astyle
 CXXFLAGS= -std=gnu++11 $(OPTIMFLAGS)
-# fastcgi++ from http://www.nongnu.org/fastcgipp
-PACKAGES= fastcgi++
-OPTIMFLAGS= -Wall -g -O #-fno-inline
-PREPROFLAGS= -D_GNU_SOURCE -I /usr/local/include $(pkg-config --cflags $(PACKAGES))
-LIBES= -L /usr/local/lib  $(pkg-config --libs $(PACKAGES)) -lboost_system -lrt -ldl
+# jsoncpp is from https://github.com/open-source-parsers/jsoncpp
+# Qt5Gui is from Qt, see http://www.qt.io/download/ & http://doc.qt.io/qt-5/
+PACKAGES= jsoncpp Qt5Gui Qt5Widgets
+## Qt5 needs -fPIC
+OPTIMFLAGS= -Wall -Wextra -g -O -fPIC #-fno-inline
+PREPROFLAGS= -D_GNU_SOURCE -I /usr/local/include $(shell pkg-config --cflags $(PACKAGES))
+LIBES= -L /usr/local/lib  $(shell pkg-config --libs $(PACKAGES)) -ldl
 SOURCES= $(wildcard iaca*.cc)
-OBJECTS= $(patsubst %.cc,%.o,$(SOURCES))
+OBJECTS= $(patsubst %.cc,%.o,$(SOURCES)) iaca.moc.cc
+QTMOC= moc
 
 .PHONY: all clean modules indent
 
@@ -33,10 +36,13 @@ all: iaca
 iaca: $(OBJECTS)
 	$(LINK.cc) -rdynamic $^ -o $@ $(LIBES)
 
+iaca.moc.cc: iaca.hh
+	$(QTMOC) $< -o $@
+
 $(OBJECTS): iaca.hh iaca.hh.gch
 
 iaca.hh.gch: iaca.hh
-	$(COMPILE.cc) $^ -c -o $@
+	$(COMPILE.cc) $(PREPROFLAGS) $^ -c -o $@
 clean:
 	$(RM) *.o *.so *~ iaca *~ core* *.gch *.orig
 
--- iacamain.cc
+++ iacamain.cc
@@ -1,6 +1,6 @@
 // file iacamain.cc
 
-// © 2012 Basile Starynkevitch
+// © 2016 Basile Starynkevitch
 //   this file iacamain.cc is part of IaCa
 //   IaCa is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
@@ -18,43 +18,16 @@
 #include "iaca.hh"
 
 using namespace Iaca;
-pthread_mutex_t MemorySegment::_mtx_ = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
-
-void* MemorySegment::allocate (size_t alsz, bool exec) throw (std::runtime_error) {
-    int logalsz = -1;
-    Locker lock(_mtx_);
-    for (int lg=5; (1L<<lg) < (long)alsz && logalsz<0; lg++)
-        if ((1L<<lg)==(long)alsz)
-            logalsz= lg;
-    assert (logalsz>0);
-    int prot = exec?(PROT_READ|PROT_WRITE):(PROT_READ|PROT_WRITE|PROT_EXEC);
-    // first, try to mmap exactly alsz, hoping that the result will be
-    // suitably aligned
-    void* ad = mmap (nullptr, alsz, prot, MAP_PRIVATE, -1, (off_t)0);
-    if (ad != MAP_FAILED && ((intptr_t)ad & ((intptr_t)alsz-1)) == 0)
-        return ad;
-    if (ad != MAP_FAILED)
-        munmap (ad, alsz);
-    // then mmap twice the space to extract aligned data
-    ad = mmap(nullptr, 2*alsz, prot, MAP_PRIVATE, -1, (off_t)0);
-    if (ad == MAP_FAILED)
-        throw std::runtime_error(std::string("mmap failed:")+strerror(errno));
-    char* endad = (char*)ad + 2*alsz;
-    intptr_t start = ((intptr_t) ad + (alsz-1)) & (~(unsigned long)(alsz-1));
-    if ((intptr_t) start > (intptr_t) ad)
-        munmap (ad, (intptr_t)start - (intptr_t)ad);
-    if ((intptr_t) start + (intptr_t)alsz < (intptr_t)endad)
-        munmap ((char*)start + alsz, (intptr_t)endad - (intptr_t) (start+alsz));
-    return (void*)start;
-}
-
-void MemorySegment::deallocate(void*ad, size_t sz) {
-    if (!ad) return;
-    Locker lock(_mtx_);
-    munmap (ad, sz);
-}
 
 int main(int argc, char**argv)
 {
-
+  QApplication app(argc,argv);
+  QApplication::setApplicationName("iaca");
+  QApplication::setApplicationVersion(version);
+  QCommandLineParser parser;
+  parser.setApplicationDescription("the IaCa system descr");
+  parser.addHelpOption();
+  parser.addVersionOption();
+  parser.process(app);
+  return app.exec();
 }
